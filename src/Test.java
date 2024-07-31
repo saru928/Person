68469f0020ba557971d7538c747cf711b28b6763
saru928 <kana.softball5@gmail.com>
2024-07-31 21:07:56 +0900

    2024/07/31

--- src/Test.java
+++ src/Test.java
@@ -8,5 +8,26 @@ public class Test {
 		
 		System.out.println(taro.name);
 		System.out.println(taro.age);
+		
+		Person jiro = new Person();
+		jiro.name = "木村次郎";
+		jiro.age = 18;
+		
+		System.out.println(jiro.name);
+		System.out.println(jiro.age);
+		
+		Person hanako = new Person();
+		hanako.name = "鈴木花子";
+		hanako.age = 16;
+		
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
+		
+		Person kana = new Person();
+		kana.name = "猿渡香菜";
+		kana.age = 23;
+		
+		System.out.println(kana.name);
+		System.out.println(kana.age);
 	}
 }
