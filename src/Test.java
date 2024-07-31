a8a2d51a6c60c9aac9e85bdec29387a2f5b0030d
saru928 <kana.softball5@gmail.com>
2024-07-31 21:58:51 +0900

    ensyu8

--- src/Test.java
+++ src/Test.java
@@ -12,6 +12,10 @@ public class Test {
 		System.out.println(taro.age);
 		System.out.println(taro.phoneNumber);
 		System.out.println(taro.address);
+		taro.talk();
+		taro.walk();
+		taro.run();
+		
 		
 		Person jiro = new Person();
 		jiro.name = "木村次郎";
@@ -23,6 +27,10 @@ public class Test {
 		System.out.println(jiro.age);
 		System.out.println(jiro.phoneNumber);
 		System.out.println(jiro.address);
+		jiro.talk();
+		jiro.walk();
+		jiro.run();
+		
 		
 		Person hanako = new Person();
 		hanako.name = "鈴木花子";
@@ -34,6 +42,9 @@ public class Test {
 		System.out.println(hanako.age);
 		System.out.println(hanako.phoneNumber);
 		System.out.println(hanako.address);
+		hanako.talk();
+		hanako.walk();
+		hanako.run();
 		
 		Person kana = new Person();
 		kana.name = "猿渡香菜";
@@ -45,6 +56,26 @@ public class Test {
 		System.out.println(kana.age);
 		System.out.println(kana.phoneNumber);
 		System.out.println(kana.address);
+		kana.talk();
+		kana.walk();
+		kana.run();
 		
+		
+		Robot aibo = new Robot();
+		aibo.name = "aibo";
+		System.out.println(aibo.name);
+		aibo.talk();
+		
+		
+		Robot asimo = new Robot();
+		asimo.name = "asimo";
+		System.out.println(asimo.name);
+		asimo.walk();
+		
+		
+		Robot pepper = new Robot();
+		pepper.name = "pepper";
+		System.out.println(pepper.name);
+		pepper.run();
 	}
 }
